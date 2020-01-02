226f20f0f50fb3753e251dc559360685b0eddc1b
brian <brian.p.coons@gmail.com>
2020-01-02 13:58:06 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TodoItem from "./todo-item"
+import "./styles.css"
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            todo: "",
+            todos: []
+        }
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:5000/todos")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    todos: data
+                })
+            })
+    }
+
+    handleChange = e => {
+        this.setState({
+            todo: e.target.value
+        })
+    }
+
+    addTodo = e => {
+        // stops the button from reloading the page
+        e.preventDefault()
+        // fetch("http://localhost:5000/todo")
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         this.setState({
+        //             todos: data
+        //         })
+        //     })
+    }
+
+    renderTodos = () => {
+        return this.state.todos.map(item => {
+            return (
+                <TodoItem key= {item.id} item={item} />
+            )
+        })
+    }
+
+    render() {
+        // console.log(this.state.todos)
+        return (
+            <div className="app">
+                <h1>ToDo List</h1>
+                <form className="add-todo" onSubmit={this.addTodo}>
+                    <input
+                        type="text"
+                        placeholder="Add Todo"
+                        onChange={this.handleChange}
+                        value= {this.state.todo}
+                    />
+                    <button type="submit">Add Todo</button>
+                </form>
+                {this.renderTodos()}
+            </div>
+        )
+    }
+}
+
+const rootElement = document.getElementById("root")
+ReactDOM.render(<App />, rootElement)
